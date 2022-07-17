4c23be962c35933545010e169066ef8a2e7e27dd
Joe Swindell <joe@reprisal.group>
2022-07-17 14:27:24 -0400

    Tune, add angular.ico for template

--- Dot-Net-Core-6-With-Angular-14/ClientApp/src/app/app.module.ts
+++ Dot-Net-Core-6-With-Angular-14/ClientApp/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AngularMaterialModule } from './angular-material.module';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -33,6 +33,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     FormsModule,
     FontAwesomeModule,
+    ReactiveFormsModule,
     AngularMaterialModule,
     AppRoutingModule,
     BrowserAnimationsModule
